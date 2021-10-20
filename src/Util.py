8e5f0b9abd516ded3f20a3dfa99309cb0f2adb6d
Mehmet Ali Erol <92267511+mehmeterol67@users.noreply.github.com>
2021-10-20 19:31:47 +0300

    String grouping (#1)
    * fix crashing problem
    
    * add comment for vibranium material
    
    * add alloyX material
    
    * typing error
    
    * refactor
    
    * refactor with f-strings

--- src/Util.py
+++ src/Util.py
@@ -1,3 +1,7 @@
 from enum import Enum
 
-materialType = Enum('Titanium', 'Carbon')
+<<<<<<< HEAD
+materialType = Enum('Titanium', 'Carbon', 'Vibranium')
+=======
+materialType = Enum('Titanium', 'Carbon', 'AlloyX')
+>>>>>>> b30d27c085bae2dd075ad8f70247146b956b88fe
